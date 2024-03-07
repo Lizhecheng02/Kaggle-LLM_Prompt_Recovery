419d5897f6d59530a3f6e2fb215d3d7e193f469b
unknown <2569618533@qq.com>
2024-03-06 21:03:29 -0800

    use base model

--- gemma-7B-2.py
+++ gemma-7B-2.py
@@ -13,13 +13,12 @@ from datasets import Dataset
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_compute_dtype=torch.bfloat16,
     # load_in_8bit=True
 )
 
-model_id = "google/gemma-7b-it"
+model_id = "google/gemma-7b"
 access_token = "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"
 tokenizer = AutoTokenizer.from_pretrained(
     model_id,
@@ -32,14 +31,13 @@ model = AutoModelForCausalLM.from_pretrained(
     quantization_config=bnb_config,
     trust_remote_code=True,
     low_cpu_mem_usage=True,
-    torch_dtype=torch.float16,
     device_map="auto",
     token=access_token
 )
 
 lora_config = LoraConfig(
-    r=32,
-    lora_alpha=16,
+    r=16,
+    lora_alpha=8,
     target_modules=[
         "q_proj", "k_proj", "v_proj",
         "o_proj", "gate_proj", "up_proj", "down_proj"
@@ -77,62 +75,41 @@ eval_dataset = Dataset.from_pandas(eval_dataset)
 
 
 def formatting_prompts_func(example):
-    output_texts = []
-    for i in range(len(example)):
-        system_prompt = f"Assuming you are an expert in self-awareness and skilled at recovering prompts. I will give you an Original Text and the Rewritten Text after I prompt you. Please tell me what prompt I most likely gave you. The length of the prompt does not exceed twenty words.\n"
-        input_prompt = f"### Original Text: {example['original_text']}\n### Rewritten Text: {example['rewritten_text']}\n"
-        output_prompt = f"### Prompt: {example['rewrite_prompt']}"
-        output_texts.append(system_prompt + input_prompt + output_prompt)
-    return output_texts
+    system_prompt = f"Assuming you are an expert in self-awareness and skilled at recovering prompts. I will give you an Original Text and the Rewritten Text after I prompt you. Please tell me what prompt I most likely gave you. The length of the prompt does not exceed thirty words.\n"
+    input_prompt = f"Original Text: {example['original_text'][0]}\nRewritten Text: {example['rewritten_text'][0]}\n"
+    output_prompt = f"Used Prompt: {example['rewrite_prompt'][0]}"
+    full_text = system_prompt + input_prompt + output_prompt
+    return [full_text]
 
 
 args = TrainingArguments(
     output_dir=f"outputs",
     gradient_accumulation_steps=8,
-    logging_steps=100,
+    logging_steps=25,
     num_train_epochs=3,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
     report_to="none",
     evaluation_strategy="steps",
-    eval_steps=100,
+    eval_steps=25,
     save_strategy="steps",
-    save_steps=100,
+    save_steps=25,
     save_total_limit=5,
     overwrite_output_dir=True,
-    load_best_model_at_end=True,
     weight_decay=0.01,
     save_only_model=True,
-    neftune_noise_alpha=1.0
-)
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-gpu_count = torch.cuda.device_count()
-print(f"Number of GPUs: {gpu_count}")
-total_steps = args.num_train_epochs * \
-    int(len(train_dataset) * 1.0 / gpu_count /
-        args.per_device_train_batch_size / args.gradient_accumulation_steps)
-scheduler = get_polynomial_decay_schedule_with_warmup(
-    optimizer,
-    num_warmup_steps=int(total_steps) * 0.05,
-    num_training_steps=total_steps,
-    power=1.5,
-    lr_end=3e-6
+    neftune_noise_alpha=1.0,
+    optim="paged_adamw_8bit"
 )
 
 torch.cuda.empty_cache()
 
 trainer = SFTTrainer(
     model=model,
-    tokenizer=tokenizer,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
     formatting_func=formatting_prompts_func,
     peft_config=lora_config,
-    dataset_num_proc=2,
-    dataset_batch_size=100,
-    max_seq_length=1024,
-    optimizers=(optimizer, scheduler),
     args=args
 )
 
